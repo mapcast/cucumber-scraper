b178dcb5d8fc47b4726391f5fb4c7cc0a2669118
mapcast <odri0220@naver.com>
2023-08-02 07:12:19 +0900

    출마표 분석 코드

--- src/main/java/com/cucumber/keiba/scrapper/scheduler/WebScrappingScheduler.java
+++ src/main/java/com/cucumber/keiba/scrapper/scheduler/WebScrappingScheduler.java
@@ -23,32 +23,142 @@ public class WebScrappingScheduler {
 	
 	private final WebScrapperUtil scrapperUtil;
 		
-	@Scheduled(cron = "0 19 21 * * *")
+	@Scheduled(cron = "0 0 7 * * *")
 	public void syncEventsInplay() {
 		WebDriver driver = scrapperUtil.getChromeDriver();
 		try {
-        	driver.get("https://race.netkeiba.com/top/race_list.html?kaisai_date=20230730");
+        	driver.get("https://race.netkeiba.com/top/race_list.html?kaisai_date=20230805");
             Thread.sleep(3000); //브라우저 로딩될때까지 잠시 기다린다.
 
             //List<WebElement> elements = driver.findElements(By.cssSelector(".Horse_Name a span"));
             List<WebElement> dataLists = driver.findElements(By.cssSelector(".RaceList_DataList"));
-            Map<String, Integer> raceMap = new HashMap<>();
+            //Map<String, Integer> raceMap = new HashMap<>();
+            List<String> raceLinks = new ArrayList<>();
             for(WebElement dataList : dataLists) {
             	List<WebElement> elements = dataList.findElements(By.cssSelector(".RaceList_DataItem a"));
             	int count = 0;
             	for(WebElement element : elements)
-            		if(element.getAttribute("href").contains("result")) count++;
-            	raceMap.put(elements.get(0).getAttribute("href"), count);
+            		if(element.getAttribute("href").contains("shutuba")) raceLinks.add(element.getAttribute("href"));
+            	//raceMap.put(elements.get(0).getAttribute("href"), count);
             }
-            for(String firstRaceLink : raceMap.keySet()) {
+            for(String raceLink : raceLinks) {
+            	driver.navigate().to(raceLink);
+            	
+            	System.out.println("레이스 정보 링크: " + raceLink);
+            	
+            	//레이스 정보 스크래핑
+            	WebElement raceNameAndGrade = driver.findElement(By.cssSelector(".RaceName"));
+            	String raceName = raceNameAndGrade.getText().replace("\"", "");
+            	System.out.println("레이스 명: " + raceName);
+            	
+            	if(raceNameAndGrade.findElements(By.cssSelector("span")).size() > 0) {
+            		String gradeIconClasses = raceNameAndGrade.findElement(By.cssSelector("span")).getAttribute("class");
+            		if(gradeIconClasses.contains("Icon_GradeType18")) {
+            			System.out.println("그레이드: 1승 클래스");
+                	} else if(gradeIconClasses.contains("Icon_GradeType17")) {
+                		System.out.println("그레이드: 2승 클래스");
+                	} else if(gradeIconClasses.contains("Icon_GradeType16")) {
+                		System.out.println("그레이드: 3승 클래스");
+                	} else if(gradeIconClasses.contains("Icon_GradeType15")) {
+                		System.out.println("그레이드: 리스티드");
+                	} else if(gradeIconClasses.contains("Icon_GradeType5")) {
+                		System.out.println("그레이드: 오픈");
+                	} else if(gradeIconClasses.contains("Icon_GradeType3")) {
+                		System.out.println("그레이드: G3");
+                	} else if(gradeIconClasses.contains("Icon_GradeType2")) {
+                		System.out.println("그레이드: G2");
+                	} else if(gradeIconClasses.contains("Icon_GradeType1")) {
+                		System.out.println("그레이드: G1");
+                	}
+            	}
+            	
+            	WebElement raceDataLine1 = driver.findElement(By.cssSelector(".RaceData01"));
+            	System.out.println("파싱 테스트: " + raceDataLine1.getText());
+            	System.out.println("마장/거리: " + raceDataLine1.findElement(By.cssSelector("span")).getText());
+            	
+            	List<WebElement> raceDataLine2Spans = driver.findElements(By.cssSelector(".RaceData02 span"));
+            	for(int i = 0; i < raceDataLine2Spans.size(); i++) {
+            		switch(i) {
+            		case 0: 
+            			System.out.println("회수: " + raceDataLine2Spans.get(i).getText());
+            			break;
+        			case 1: 
+        				System.out.println("경기장: " + raceDataLine2Spans.get(i).getText());
+        				break;
+        			case 2: 
+        				System.out.println("회차: " + raceDataLine2Spans.get(i).getText());
+        				break;
+        			case 3: 
+        				System.out.println("마령/조건: " + raceDataLine2Spans.get(i).getText());
+        				break;
+        			case 4: 
+        				System.out.println("클래스: " + raceDataLine2Spans.get(i).getText());
+        				break;
+        			case 6: 
+        				System.out.println("핸디캡: " + raceDataLine2Spans.get(i).getText());
+        				break;
+        			case 7:
+        				System.out.println("출주두수: " + raceDataLine2Spans.get(i).getText());
+        				break;
+        			case 8: 
+        				System.out.println("본상금: " + raceDataLine2Spans.get(i).getText());
+        				break;
+            		}
+            	}
+            	
+            	
+            	
+            	Thread.sleep(5000);
+            	//출주마 목록 스크래핑
+            	List<WebElement> horses = driver.findElements(By.cssSelector(".HorseList"));
+            	for(WebElement horse : horses) {
+            		List<WebElement> horseDatas = horse.findElements(By.cssSelector("td"));
+            		for(int i = 0; i < horseDatas.size(); i++) {
+            			switch(i) {
+                			case 0: break;
+                			case 1: break;
+                			case 3: 
+                				WebElement horseName = horseDatas.get(i).findElement(By.cssSelector("a"));
+                				System.out.println("출주마 정보 링크: " + horseName.getAttribute("href"));
+                				System.out.println("마명: " + horseName.getText().replace("\"", ""));
+                				break;
+                			case 4: 
+                				System.out.println("성별/나이: " + horseDatas.get(i).getText());
+                				break;
+                			case 5: 
+                				System.out.println("근량: " + horseDatas.get(i).getText());
+                				break;
+                			case 6: 
+                				System.out.println("기수: " + horseDatas.get(i).findElement(By.cssSelector("a")).getText());
+                				break;
+                			case 7: 
+                				WebElement home = horseDatas.get(i);
+                				System.out.println("지역: " + home.findElement(By.cssSelector("span")).getText());
+                				System.out.println("구사: " + home.findElement(By.cssSelector("a")).getText());
+                				break;
+                			case 8: 
+                				//System.out.println("마체중: " + horseDatas.get(j).getText());
+                				break;
+                			case 9: 
+                				System.out.println("예상배당: " + horseDatas.get(i).findElement(By.cssSelector("span")).getText());
+                				break;
+                			case 10: 
+                				System.out.println("인기: " + horseDatas.get(i).findElement(By.cssSelector("span")).getText());
+                				break;
+                			default: break;
+            			}
+            		}
+            	}
+            	Thread.sleep(3000);
+            	/*
             	System.out.println("레이스 수: " + raceMap.get(firstRaceLink));
-                for(int i = 1; i <= raceMap.get(firstRaceLink); i++) {
+                for(int i = 1; i <= 12; i++) {
                 	String raceNumber = Integer.toString(i);
                 	if(raceNumber.length() == 1) raceNumber = "0" + raceNumber;
                     String modified = firstRaceLink.substring(0, firstRaceLink.length() - 15) + raceNumber + "&rf=race_list";
                 	driver.navigate().to(modified);
-                    Thread.sleep(2000);
                 }
+                */
             }
             
             
